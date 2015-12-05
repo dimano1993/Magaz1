5390fcdb641320b8818b74c3c6e5faf5ee1961d2
Goloveynatasha <Goloveynatasha@yandex.ru>
2015-12-05 15:19:02 +0600

    Теперь admin может удалять заказы пользователей.

--- moiadmin/urls.py
+++ moiadmin/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', 'moiadmin.views.show_users', name='show_users'),
-    url(r'^(?P<user_id>[0-9]+)$', 'moiadmin.views.conkr_user',  name='conkr_users'),
+    url(r'^(?P<user_id>[0-9]+)$', 'moiadmin.views.conkr_user',  name='conkr_user'),
+    url(r'^(?P<zak_id>[0-9]+)/del$', 'moiadmin.views.delete',  name='delete'),
 
 ]
--- moiadmin/views.py
+++ moiadmin/views.py
@@ -1,6 +1,6 @@
 from django.contrib import auth
 from django.contrib.auth.models import User
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template.context_processors import csrf
 from basket.models import Otlojit
 
@@ -19,7 +19,17 @@ def conkr_user(request, user_id):
     preuser = User.objects.get(id=user_id)
     user = preuser.username
     args["user"] = user
-    #user = auth.get_user()
+    # user = auth.get_user()
     args["username"] = auth.get_user(request).username
     args["zakaz"] = Otlojit.objects.filter(user=user)
     return render_to_response('modul/conkruser.html', args)
+
+
+def delete(request, zak_id):
+    if request.POST:
+        b = Otlojit.objects.get(id=zak_id)
+        prepreuser = b.user
+        preuser = User.objects.get(username=prepreuser)
+        user = preuser.id
+        b.delete()
+    return redirect('/show_users/%s' % user)
