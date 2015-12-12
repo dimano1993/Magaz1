887d582f16cdd6a7ce0d61bcb3ea8e4620eb31c9
Дмитрий <dimano1993@rambler.ru>
2015-12-12 15:24:50 +0600

    Добавил тест.

--- basket/tests.py
+++ basket/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from basket.models import notebook
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    # Тест на проверку работоспособности домашней страницы
+    def test_homepage_available(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEquals(response.status_code, 200)
